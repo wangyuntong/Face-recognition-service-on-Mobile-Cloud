b08679dfb2a29937733dc676877e5c55689fe65e
wangyuntong <wangyuntongus@yeah.net>
2016-05-04 02:53:49 -0400

    js modified

--- js/index.js
+++ js/index.js
@@ -1,11 +1,95 @@
 /**
  * Created by yuntongwang on 4/18/16.
  */
-var spawn = require('child_process').spawn
 
-execPath = '/home/ubuntu/torch/install/bin/th'
-runScriptName = '/home/ubuntu/Face-recognition-service-on-Mobile-Cloud/run.lua'
-loadScriptName = '/home/ubuntu/Face-recognition-service-on-Mobile-Cloud/load.lua'
+var http = require('http')
+var spawn = require('child_process').spawn
+var fs = require('fs')
+
+// execPath = '/home/ubuntu/torch/install/bin/th'
+execPath = '/Users/yuntongwang/torch/install/bin/th'
+runScriptName = '../run.lua'
+loadScriptName = '../load.lua'
+
+const luaScript = spawn(execPath, [loadScriptName]);
+
+luaScript.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+});
+
+luaScript.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+});
+
+luaScript.on('close', function (code) {
+    console.log('child process exited with code ' + code);
+});
+
+http.createServer(function(request, response) {
+    var imagedata = []
+    var imgID = request.get('imageID');
+    var method = request.method;
+    var url = request.url;
+    var body = [];
+    response.setEncoding('binary')
+
+    request.on('error', function(err) {
+        console.error(err);
+    }).on('data', function(chunk) {
+        body.push(chunk);
+    }).on('end', function() {
+
+        // Write the image to current image file
+        var FRresult = [];
+        fs.writeFile('../image/currentImage.jpg', imagedata, 'binary', function(err){
+            if (err) throw err
+            console.log('File saved.')
+            const luaRunScript = spawn(execPath, [runScriptName]);
+
+            luaRunScript.stdout.on('data', function (data) {
+                console.log('stdout: ' + data);
+                FRresult.push(data)
+            });
+
+            luaRunScript.stderr.on('data', function (data) {
+                console.log('stderr: ' + data);
+            });
+
+            luaRunScript.on('close', function (code) {
+                console.log('child process exited with code ' + code);
+            });
+
+        })
+
+        //
+        //
+
+
+        response.on('error', function(err) {
+            console.error(err);
+        });
+
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'application/json');
+        // Note: the 2 lines above could be replaced with this next one:
+        // response.writeHead(200, {'Content-Type': 'application/json'})
+
+        var responseBody = {
+            headers: headers,
+            method: method,
+            url: url,
+            body: FRresult
+        };
+
+        response.write(JSON.stringify(responseBody));
+        response.end();
+        // Note: the 2 lines above could be replaced with this next one:
+        // response.end(JSON.stringify(responseBody))
+
+        // END OF NEW STUFF
+    });
+}).listen(8080);
+
 
 //const loadLuaScript = spawn(execPath, [loadScriptName]);
 
@@ -21,16 +105,3 @@ loadScriptName = '/home/ubuntu/Face-recognition-service-on-Mobile-Cloud/load.lua
 //    console.log('child process exited with code ' + code);
 //});
 
-const luaScript = spawn(execPath, [runScriptName]);
-
-luaScript.stdout.on('data', function (data) {
-    console.log('stdout: ' + data);
-});
-
-luaScript.stderr.on('data', function (data) {
-    console.log('stderr: ' + data);
-});
-
-luaScript.on('close', function (code) {
-    console.log('child process exited with code ' + code);
-});
--- run.lua
+++ run.lua
@@ -1,4 +1,4 @@
-require 'nngraph' 
+require 'nngraph'
 require 'cunn'
 require 'image'
 
@@ -9,21 +9,20 @@ local scaleDownF = 0.8
 local path = '/home/ubuntu/Face-recognition-service-on-Mobile-Cloud'
 --model config
 local numDet = 14 --number of detectors
-local rfW = {12,16,20,24,28,32,36,40,44,48,52,56,60,64} --RFs
-local skp = {1,1,1,1,2,2,2,2,2,2,2,4,4,4} --skip vals
+local rfW = { 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64 } --RFs
+local skp = { 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 4, 4, 4 } --skip vals
 local nnF = path .. '/facedet.nn'
 
 --FDDB images
-local maxW   = 240
-local minW   = 68
-local dataPx = path .. '/FDDB/FDDB-folds/FDDB-fold-'
+local maxW = 240
+local minW = 68
 local numFld = 10
-local imgSrc = path .. '/FDDB/'
+local imgSrc = path .. '/image/'
 
-local net   = torch.load(nnF)
-net   = net:cuda()
+local net = torch.load(nnF)
+net = net:cuda()
 
-function NMS (boxT, overlapT)
+function NMS(boxT, overlapT)
 
     local retT = {}
 
@@ -33,23 +32,23 @@ function NMS (boxT, overlapT)
     end
 
     local numRect = boxT:size(1)
-    assert( boxT:size(2) == 5 )
+    assert(boxT:size(2) == 5)
 
-    local maskT = torch.ByteTensor( numRect ):fill(1)
+    local maskT = torch.ByteTensor(numRect):fill(1)
 
-    local identT = torch.IntTensor( numRect )
-    for ii=1,numRect do identT[ii] = ii end
+    local identT = torch.IntTensor(numRect)
+    for ii = 1, numRect do identT[ii] = ii end
 
     --grab the coordinates of the bounding boxes
-    local x1 = boxT[{ {}, {1} }]
-    local y1 = boxT[{ {}, {2} }]
-    local x2 = x1 + boxT[{ {}, {3} }]
-    local y2 = y1 + boxT[{ {}, {4} }]
-    local detV = boxT[{ {}, {5} }] --detection scores
+    local x1 = boxT[{ {}, { 1 } }]
+    local y1 = boxT[{ {}, { 2 } }]
+    local x2 = x1 + boxT[{ {}, { 3 } }]
+    local y2 = y1 + boxT[{ {}, { 4 } }]
+    local detV = boxT[{ {}, { 5 } }] --detection scores
 
     --compute the area of the bounding boxes and sort the bounding
     --boxes by the bottom-right y-coordinate of the bounding box
-    local area = torch.cmul((x2 - x1 + 1) , (y2 - y1 + 1))
+    local area = torch.cmul((x2 - x1 + 1), (y2 - y1 + 1))
     -- sortV, sortI = torch.sort(y2, 1, true) --true causes descending sort
 
     --keep looping while some indexes still remain in the indexes list
@@ -63,22 +62,22 @@ function NMS (boxT, overlapT)
         --     thisI = sortI[ii][1]
         --     if (maskT[thisI] == 1) then break end
         -- end
-	--grab largest remaining score
-	local maxScr = -10000.0;
-	for ii=1,numRect do
-	    if ( (maskT[ii] == 1) and (detV[ii][1] > maxScr) ) then
-		thisI = ii
-		maxScr = detV[ii][1]
-	    end
-	end
+        --grab largest remaining score
+        local maxScr = -10000.0;
+        for ii = 1, numRect do
+            if ((maskT[ii] == 1) and (detV[ii][1] > maxScr)) then
+                thisI = ii
+                maxScr = detV[ii][1]
+            end
+        end
 
         maskT[thisI] = 0
         numProc = numProc + 1
 
-	if (numProc == numRect) then
-	    table.insert(retT, thisI)
-	    break
-	end
+        if (numProc == numRect) then
+            table.insert(retT, thisI)
+            break
+        end
 
         --find the largest (x, y) coordinates for the start of
         --the bounding box and the smallest (x, y) coordinates
@@ -96,16 +95,16 @@ function NMS (boxT, overlapT)
 
         --compute the ratio of overlap
         -- local overlap = torch.cdiv( torch.cmul(thisW,thisH), area[maskT])
-	local overlapA = torch.cmul(thisW,thisH)
-        local overlap1 = torch.cdiv( overlapA, area[maskT] )
-        local overlap2 = overlapA / area[thisI][1] 
-	local overlap  = torch.cmax( overlap1, overlap2 )
+        local overlapA = torch.cmul(thisW, thisH)
+        local overlap1 = torch.cdiv(overlapA, area[maskT])
+        local overlap2 = overlapA / area[thisI][1]
+        local overlap = torch.cmax(overlap1, overlap2)
 
         --delete all indexes from the index list that have
         --larger than threshold overlap
         local maxScr = detV[thisI][1]
         local maxIdx = thisI
-        for ii=1,overlap:size(1) do
+        for ii = 1, overlap:size(1) do
             local gIdx = idRM[ii]
             if (overlap[ii] > overlapT) then
                 if (detV[gIdx][1] > maxScr) then
@@ -116,122 +115,103 @@ function NMS (boxT, overlapT)
                 numProc = numProc + 1
             end
         end
-        table.insert(retT,maxIdx)
-
+        table.insert(retT, maxIdx)
     end
 
     --return only the bounding boxes that were picked using the
     --integer data type
     return retT
 end
+--
+--function findFaces(imgG, scale, detT)
+--
+--    local newH = math.ceil(imgG:size(2) * scale)
+--    local newW = math.ceil(imgG:size(3) * scale)
+--    local imgS = image.scale(imgG, newW, newH)
+--
+--    imgS:add(-0.5)
+--
+--    imgS = imgS:double()
+--    imgS = imgS:cuda()
+--
+--    local prediction = net:forward(imgS)
+--
+--    --detected faces
+--    for dl = 1, numDet do
+--
+--        local tP = prediction[dl][{ { 1 }, {}, {} }]
+--        local tN = prediction[dl][{ { 2 }, {}, {} }]
+--        local tD = tP - tN
+--        local logZ = tN + torch.log1p(torch.exp(tD))
+--
+--        --log prob
+--        tP = tP - logZ
+--
+--        local hVal = math.ceil(rfW[dl] / scale)
+--
+--        local numR = tP:size(2)
+--        local numC = tP:size(3)
+--
+--        local ix = 0
+--        local iy = 0
+--        tP:apply(function(x)
+--            ix = ix % numC + 1
+--            if (ix == 1) then iy = iy + 1 end
+--            if (x > detThresh) then
+--                xVal = math.ceil(((ix - 1) * skp[dl] + 1) / scale)
+--                yVal = math.ceil(((iy - 1) * skp[dl] + 1) / scale)
+--                table.insert(detT, { xVal, yVal, hVal, hVal, x })
+--            end
+--            return x
+--        end)
+--    end
+--
+--    return table.getn(detT)
+--end
 
-function findFaces (imgG, scale, detT)
+local imgF = imgSrc .. 'currentImage.jpg'
+local img = image.load(imgF)
 
-    local newH = math.ceil(imgG:size(2) * scale)
-    local newW = math.ceil(imgG:size(3) * scale)
-    local imgS = image.scale(imgG,newW,newH)
-
-    imgS:add(-0.5)
-
-    imgS = imgS:double()
-    imgS = imgS:cuda()
-   
-    local prediction = net:forward(imgS)
-
-    --detected faces
-    for dl=1,numDet do
-
-        local tP = prediction[dl][{{1},{},{}}]
-        local tN = prediction[dl][{{2},{},{}}]
-        local tD = tP - tN
-        local logZ = tN + torch.log1p(torch.exp(tD))
-
-	--log prob
-        tP = tP - logZ
-
-	local hVal = math.ceil( rfW[dl] / scale )
-
-	local numR = tP:size(2)
-	local numC = tP:size(3)
-
-	local ix = 0
-	local iy = 0
-	tP:apply( function (x)
-		ix = ix%numC + 1
-		if (ix == 1) then iy = iy + 1 end
-		if ( x > detThresh ) then
-		    xVal = math.ceil( ((ix-1)*skp[dl] + 1) / scale )
-		    yVal = math.ceil( ((iy-1)*skp[dl] + 1) / scale )
-		    table.insert(detT, {xVal, yVal, hVal, hVal, x})
-		end
-	        return x
-	end )
-    end
-    
-    return table.getn(detT)
-
+local imgG = img
+if (img:size(1) < 3) then
+    imgG = torch.Tensor(3, imgG:size(2), imgG:size(3))
+    imgG[{ { 1 }, {}, {} }] = img[{ { 1 }, {}, {} }]
+    imgG[{ { 2 }, {}, {} }] = img[{ { 1 }, {}, {} }]
+    imgG[{ { 3 }, {}, {} }] = img[{ { 1 }, {}, {} }]
 end
 
--- for fold=1,numFld do
-local numImgProc=0
-for fold=1,2 do
-
-    local dataF = dataPx .. string.format('%02d',fold) .. '.txt'
-    local line= ''
-    for line in io.lines(dataF) do
-
-        line = string.gsub(line,'\n','')
-        line = string.gsub(line,'\r','')
-
-	local imgF = imgSrc .. line .. '.jpg'
-        local img = image.load(imgF)
-
-        local imgG = img
-	if (img:size(1) < 3) then
-	    imgG = torch.Tensor(3,imgG:size(2),imgG:size(3))
-	    imgG[{{1},{},{}}] = img[{{1},{},{}}]
-	    imgG[{{2},{},{}}] = img[{{1},{},{}}]
-	    imgG[{{3},{},{}}] = img[{{1},{},{}}]
-	end
-
-	--check imgG shape is ok
-	local maxDim = imgG:size(2)
-        local minDim = -1
-	if (imgG:size(3) > maxDim) then
-	    minDim = maxDim
-	    maxDim = imgG:size(3)
-	else
-	    minDim = imgG:size(3)
-	end
-	
-	local scale = (maxW / maxDim)
-	if ( scale > 1.0 ) then
-	    scale = 1.0
-	end
-
-	local detT = {}
-	local detN = 0
-	while ( minDim*scale >= minW ) do
-	    detN = findFaces(imgG, scale, detT)
-	    scale = scale * scaleDownF
-	end
-	
-	--run NMS once again to remove duplicates introduced at various scales
-	local detTT = torch.Tensor(detT)
-	local nmsT = NMS(detTT, nmsThresh)
-	local nDet = table.getn(nmsT)
-
-	io.write( line .. '\n' )
-	io.write( nDet .. '\n' )
-	for dd in pairs(nmsT) do
-	    local thisidx = nmsT[dd]
-	    io.write( detTT[thisidx][1] .. ' ' .. detTT[thisidx][2] .. ' ' .. detTT[thisidx][3] .. ' ')
-	    io.write( detTT[thisidx][4] .. ' ' .. detTT[thisidx][5] .. '\n')
-	end
-	io.flush()
-
-	numImgProc = numImgProc + 1
-
-    end
-
+--check imgG shape is ok
+local maxDim = imgG:size(2)
+local minDim = -1
+if (imgG:size(3) > maxDim) then
+    minDim = maxDim
+    maxDim = imgG:size(3)
+else
+    minDim = imgG:size(3)
 end
+
+local scale = (maxW / maxDim)
+if (scale > 1.0) then
+    scale = 1.0
+end
+
+local detT = {}
+local detN = 0
+while (minDim * scale >= minW) do
+    detN = findFaces(imgG, scale, detT)
+    scale = scale * scaleDownF
+end
+
+--run NMS once again to remove duplicates introduced at various scales
+local detTT = torch.Tensor(detT)
+local nmsT = NMS(detTT, nmsThresh)
+local nDet = table.getn(nmsT)
+
+io.write(line .. '\n')
+io.write(nDet .. '\n')
+for dd in pairs(nmsT) do
+    local thisidx = nmsT[dd]
+    io.write(detTT[thisidx][1] .. ' ' .. detTT[thisidx][2] .. ' ' .. detTT[thisidx][3] .. ' ')
+    io.write(detTT[thisidx][4] .. ' ' .. detTT[thisidx][5] .. '\n')
+end
+io.flush()
