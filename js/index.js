95ad46316336f1851df39097dc17699a3de40059
Ubuntu <ubuntu@ip-172-31-59-102.ec2.internal>
2016-05-04 08:13:51 +0000

    debugging...

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 /**
  * Created by yuntongwang on 4/18/16.
  */
+console.log("server start ... ");
 
 var http = require('http')
 var spawn = require('child_process').spawn
 var fs = require('fs')
 
-// execPath = '/home/ubuntu/torch/install/bin/th'
-execPath = '/Users/yuntongwang/torch/install/bin/th'
+execPath = '/home/ubuntu/torch/install/bin/th'
+// execPath = '/Users/yuntongwang/torch/install/bin/th'
 runScriptName = '../run.lua'
 loadScriptName = '../load.lua'
 
@@ -22,33 +23,58 @@ luaScript.stderr.on('data', function (data) {
 });
 
 luaScript.on('close', function (code) {
-    console.log('child process exited with code ' + code);
+    console.log('Load model child process exited with code ' + code);
 });
 
-http.createServer(function(request, response) {
+var server = http.createServer(function(request, response) {
+    console.log("Processing incoming image...");
     var imagedata = []
-    var imgID = request.get('imageID');
+    var headers = request.headers;
     var method = request.method;
     var url = request.url;
-    var body = [];
-    response.setEncoding('binary')
 
     request.on('error', function(err) {
         console.error(err);
     }).on('data', function(chunk) {
-        body.push(chunk);
+        imagedata.push(chunk);
     }).on('end', function() {
 
         // Write the image to current image file
         var FRresult = [];
         fs.writeFile('../image/currentImage.jpg', imagedata, 'binary', function(err){
-            if (err) throw err
-            console.log('File saved.')
+            if (err) {
+                return console.error(err);
+            }
+            console.log('File saved in ../image/currentImage.jpg')
             const luaRunScript = spawn(execPath, [runScriptName]);
 
             luaRunScript.stdout.on('data', function (data) {
                 console.log('stdout: ' + data);
                 FRresult.push(data)
+                
+                response.on('error', function(err) {
+                    console.error(err);
+                });
+
+                response.statusCode = 200;
+                response.setHeader('Content-Type', 'application/json');
+                // Note: the 2 lines above could be replaced with this next one:
+                // response.writeHead(200, {'imageID': imgID})
+
+                var responseBody = {
+                  headers: headers,
+                  method: method,
+                  url: url,
+                  body: FRresult
+                };
+
+                response.write(JSON.stringify(responseBody));
+                response.end();
+                console.log("reponse sent back to mobile ... ");
+                // Note: the 2 lines above could be replaced with this next one:
+                // response.end(JSON.stringify(responseBody))
+
+                // END OF NEW STUFF
             });
 
             luaRunScript.stderr.on('data', function (data) {
@@ -61,47 +87,11 @@ http.createServer(function(request, response) {
 
         })
 
-        //
-        //
-
-
-        response.on('error', function(err) {
-            console.error(err);
-        });
-
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'application/json');
-        // Note: the 2 lines above could be replaced with this next one:
-        // response.writeHead(200, {'Content-Type': 'application/json'})
-
-        var responseBody = {
-            headers: headers,
-            method: method,
-            url: url,
-            body: FRresult
-        };
-
-        response.write(JSON.stringify(responseBody));
-        response.end();
-        // Note: the 2 lines above could be replaced with this next one:
-        // response.end(JSON.stringify(responseBody))
-
-        // END OF NEW STUFF
     });
-}).listen(8080);
+});
 
+// server listen on port 8080
+port = '80'
+server.listen(port)
 
-//const loadLuaScript = spawn(execPath, [loadScriptName]);
-
-//loadLuaScript.stdout.on('data', function (data) {
-//    console.log('stdout: ' + data);
-//});
-
-//loadLuaScript.stderr.on('data', function (data) {
-//    console.log('stderr: ' + data);
-//});
-
-//loadLuaScript.on('close', function (code) {
-//    console.log('child process exited with code ' + code);
-//});
-
+console.log("Server listen on port " + port);
